7f794e13931ec95531a74fb0f7af7a594c221eef
jyothishkumarav <jyothishkumarav@gmail.com>
2018-03-22 14:03:43 +0530

    Adding report and multiintsance driver

--- conf.js
+++ conf.js
@@ -1,13 +1,40 @@
+var Jasmine2HtmlReporter = require('protractor-jasmine2-html-reporter');
+
 exports.config = {
   seleniumAddress: 'http://localhost:4444/wd/hub',
-  multiCapabilities:[
+  /*multiCapabilities:[
    { browserName: 'chrome'},
    { browserName: 'firefox'}
-  ],
+  ],*/
+  capabilities: {
+    browserName: 'chrome',
+    shardTestFiles: true,
+    maxInstances: 4
+  },
+  suites: {
+    homepage: './test/*spec.js',
+    search: ['./home/*spec.js',
+      './todo/*spec.js'
+    ]
+  },
   framework: 'jasmine',
-  specs: ['todo-spec.js'],
+  // specs: ['todo-spec.js'],
   jasmineNodeOpts: {
     defaultTimeoutInterval: 30000
+  },
+  onPrepare: function () {
+    browser.driver.manage().window().maximize();
+    jasmine.getEnv().addReporter(
+      new Jasmine2HtmlReporter({
+        savePath: './report',
+        screenshotsFolder: './images',
+        takeScreenshots: true,
+        cleanDestination: true,
+        fileName: 'MyReportName',
+        fileNameDateSuffix: true,
+        consolidate: true,
+        consolidateAll: true
+      })
+    )
   }
 };
-
